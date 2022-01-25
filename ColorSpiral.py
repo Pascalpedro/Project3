8f31317db1c028a188bda5802b815bd0581732b2
Attama Pascal <Attamapascalpedro@gmail.com>
2022-01-25 14:09:14 +0100

    Python Turtle Graphics

---
+++ ColorSpiral.py
@@ -0,0 +1,13 @@
+import turtle
+
+colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow']
+t = turtle.Pen()
+t.speed(10)
+turtle.bgcolor('Black')
+for x in range(360):
+    t.pencolor(colors[x% len(colors)])
+    t.width(x/100+1)
+    t.forward(x)
+    t.left(59)
+
+turtle.done()
